624875bd80d7f834055407539ceeeb19d94c76ee
alec wandy <47435824+cadazzles@users.noreply.github.com>
2026-04-15 16:33:37 -0700

    Nicer formatting for console output

--- logparser.py
+++ logparser.py
@@ -81,7 +81,7 @@ def extract_data(log_file):
 def print_results_to_console(results):
     """ Print results to console with indicators for which data corresponds to which relevant field """
     for timestamp, username, ip in results:
-        print(f'Access Time/Date: {timestamp}, Username: {username}, Source IP Address: {ip}')
+        print(f'Access Time/Date: {timestamp} | Username: {username:<13} | Source IP Address: {ip}')
     print(f'Total failed attempts: {len(results)}')
 
 def export_results_to_csv(results, output_csv):
