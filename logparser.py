e6dc6623587fc7bd579f6481f0d940db796293b4
alec wandy <47435824+cadazzles@users.noreply.github.com>
2026-04-15 16:28:47 -0700

    Spacing adjustments

--- logparser.py
+++ logparser.py
@@ -29,36 +29,32 @@ def main():
 def parse_args():
     """ Exception handling for command-line arguments """
     try:
-        # Attempt to set log file for analysis as first argument, otherwise error out if no argument is given
+        # Attempt to use log file from specified path for analysis - error out if nothing is supplied by user
         log_file = sys.argv[1]
-        # If a name for the output csv is provided, use it - otherwise, use a default name of "output.csv"
-        output_csv = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_CSV_FILENAME
-        
-        # Run validation checks on the given log file
-        validate_log_file(log_file)
-
-        # Make sure the output csv ends with a .csv extension; add it if it doesn't have one
-        if not output_csv.endswith(".csv"):
-            output_csv = output_csv + ".csv"
-
-        return log_file, output_csv
     except IndexError:
-        # Exit with error code 1 and print help message if no log file is provided
+        # Exit with error code 1 and print help message if no arguments are provided
         print("Usage: python3 logparser.py <path_to_log_file> <optional: path_to_output_csv>")
         sys.exit(1)
 
+    # If an argument specifying the path for the output csv is provided, use it - otherwise, use default name in working dir
+    output_csv = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_CSV_FILENAME  
+    # Run validation checks on the given log file
+    validate_log_file(log_file)
+    # Make sure the output csv ends with a .csv extension; add it if it doesn't have one
+    if not output_csv.endswith(".csv"):
+        output_csv = output_csv + ".csv"
+    return log_file, output_csv
+
 def validate_log_file(log_file):
-    """ Validates that the log file actually exists and contains data to be analyzed """
+    """ Validates that the log file actually exists and contains data to be analyzed - immediately quit if validation fails """
     # Ensure that the path to the log file provided is actually an existing file
     if not os.path.isfile(log_file):
         print(f'Error: {log_file} does not exist. Try another file.')
         sys.exit(1)
-
     # Ensure that the log file provided is not completely empty (i.e. size = 0 bytes)
     if os.path.getsize(log_file) == 0:
         print(f'Error: auth.log file {log_file} is empty. Try another file.')
         sys.exit(1)
-
     # Ensure that the log file provided does not only contain whitespace
     with open(log_file, 'r') as log:
         if not log.read().strip():
@@ -75,14 +71,11 @@ def extract_data(log_file):
             # Only work on lines containing "failed password", as these are the lines we are primarily concerned with
             if "failed password" not in line.lower():
                 continue
-
             # regex matching: isolates date/time, username, and source ip address from each line. accounts for instances of "invalid user"
-            matched_string = re.findall(FAILED_LOGIN_REGEX, line)
-                
+            matched_string = re.findall(FAILED_LOGIN_REGEX, line) 
             # if the pattern is successfully matched + isolated, add it to our list of failed logins
             if matched_string:
                 failed_logins += matched_string
-    
     return failed_logins
 
 def print_results_to_console(results):
@@ -96,15 +89,12 @@ def export_results_to_csv(results, output_csv):
     # Temporary list containing the header/footer information for the CSV. This gets prepended/appended to the results that get exported to CSV.
     csv_header = [('Access Time/Date', 'Username', 'Source IP Address')]
     csv_footer = [('Total failed attempts', len(results), '')]
-
     # This is NOT fast - it's O(n) - but it does the job on modern CPUs because our list of results has less than 1000 entries
     csv_contents = csv_header + results + csv_footer
-    
     # Export results to CSV file
     with open(output_csv, 'w', newline='') as output:
         csv_writer = csv.writer(output)
         csv_writer.writerows(csv_contents)
-    
     # Inform the user that a CSV version of the results was created
     print(f'A CSV version of the results was outputted to {output_csv}.')
 
