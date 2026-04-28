c14a1450c704d018095a1b05fa64dcda330097f1
alec wandy <47435824+cadazzles@users.noreply.github.com>
2026-04-27 22:03:40 -0700

    Add check for compatible platforms

--- sysinfo.py
+++ sysinfo.py
@@ -7,8 +7,21 @@
 # Licensed under the MIT License (https://opensource.org/license/mit)
 # AlecWandy-20260427: Initial version
 
+# Imports
+import sys
+
+# Constants
+DEFAULT_OUTPUT_FILE = "sysinfo"
+
 def main():
-    """Primary script entry point - handles parsed cmd arguments and passes them to information gathering functions before outputting """
+    """ Primary script entry point - handles parsed cmd arguments and passes them to information gathering functions before outputting """
+    check_compatibility()
+
+def check_compatibility():
+    """ Basic script compatibility check. If the host system is not one of the supported three, immediately quit with an error. """
+    if not sys.platform in ["win32", "darwin", "linux"]:
+        print(f'ERROR: platform {sys.platform} is not supported by sysinfo. Please run this script from a Windows, macOS, or Linux system.')
+        sys.exit(1)
 
 # Run main() if called directly from cmd, otherwise function as an importable library
 if __name__ == '__main__':
