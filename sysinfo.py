6eb5950d1743590ac5cc26746465b0d3324815aa
alec wandy <47435824+cadazzles@users.noreply.github.com>
2026-04-28 00:21:58 -0700

    Use elif instead of multiple ifs

--- sysinfo.py
+++ sysinfo.py
@@ -85,11 +85,11 @@ def get_os_info():
             # Retrieves the following information for Windows-based systems
             # Windows name (i.e. Windows), marketing version (i.e "11"), feature edition (i.e. Professional), build version (i.e. 26200), and OS architecture (i.e. AMD64)
             os_info.extend([platform.system(), platform.release(), platform.win32_edition(), platform.version(), platform.machine()])
-        if CLIENT_PLATFORM == "darwin":
+        elif CLIENT_PLATFORM == "darwin":
             # Retrieves the following information for macOS-based systems
             # macOS, macOS version (i.e. "26.4.1"), Darwin version (i.e. "25.4"), OS architecture (i.e. arm64)
             os_info.extend(["macOS", platform.mac_ver()[0], platform.version(), platform.machine()])
-        if CLIENT_PLATFORM == "linux":
+        elif CLIENT_PLATFORM == "linux":
             # Retrieves the following information for Linux-based systems
             # Distribution name (i.e. Ubuntu Server), Distribution version (i.e. 24.04.1 Noble Numbat), Linux kernel version (i.e. 6.6.89-ubuntu-1-1), OS architecture (i.e. AMD64)
             os_info.extend([distro.name(pretty=True), distro.version(pretty=True, best=True), platform.version(), platform.machine()])
