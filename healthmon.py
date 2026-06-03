43687c45ef55b40843b8b5b30950fe513fd611f1
alec wandy <47435824+cadazzles@users.noreply.github.com>
2026-06-02 19:38:33 -0700

    Initial script implementation

--- healthmon.py
+++ healthmon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# Uses threshold and service definitions in a config.json file to watch and alert for anomalies in CPU/memory usage load and service health.
-# All output is written to log files via the Python logging module, and alerts (i.e. exceeded thresholds) are outputted to /var/log/syslog.
+# Uses threshold and service definitions in a config.json file to watch and alert for anomalies in CPU/memory/disk usage load and service health.
+# All output is written to log files via the Python logging module, and alerts (i.e. exceeded thresholds) are also outputted to /var/log/syslog.
 # System information is retrieved using sysinfo.py (Sprint 2), included as a snapshotted/modified version in the repository. This script is
 # designed to be run on a schedule via cron or another scheduled run utility.
 #
@@ -12,8 +12,10 @@
 import json
 import argparse
 import sys
+import subprocess
 import logging
-from sysinfo import get_cpu_info, get_mem_info
+from logging.handlers import SysLogHandler
+from sysinfo import get_cpu_info, get_mem_info, get_disk_info
 
 # Constants
 
@@ -24,14 +26,25 @@ logger.setLevel(logging.INFO)
 log_format = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 
 def main():
-    """ Primary script entry point - handles parsed cmd arguments and passes them to recon functions before outputting. """
-    parse_args()
+    """ Primary script entry point - handles parsed cmd arguments and runs necessary functions prior to logging. """
+    check_if_os_supported()
+    config, is_verbose_mode = parse_args()
+    create_log_handlers(config, is_verbose_mode)
+    health_stats = collect_all(config)
+    log_health_events(config, health_stats)
 
-def check_if_os_linux():
-    """ Check if the current OS is Linux-based, as this script only supports Linux systems for logging. """
+
+def check_if_os_supported():
+    """ Check if the current OS is Linux-based & using systemd, as this script only supports Linux + systemd systems for logging. """
     if not sys.platform in ["linux"]:
         logger.error(f'ERROR: platform {sys.platform} is not supported by healthmon. Please run this script from a Linux system.')
         sys.exit(1)
+    else:
+        try:
+            subprocess.run('systemctl')
+        except FileNotFoundError:
+            logger.error(f'ERROR: This system is not using systemd for init and is not supported. Please run this script from a Linux system with systemd.')
+            sys.exit(1)
 
 def parse_args():
     """ Parses arguments taken from the command line, including exception handling for missing/incorrect arguments. """
@@ -52,8 +65,100 @@ def parse_args():
     
     return config, args.check
 
+def get_cpu_health(config):
+    current_cpu_stats = get_cpu_info()
+    cpu_load = current_cpu_stats['load_1_min']
+    if cpu_load > config['checks']['cpu_load_1min']:
+        is_cpu_usage_above_threshold = True
+    else:
+        is_cpu_usage_above_threshold = False
+    
+    return {
+        'cpu_load': cpu_load,
+        'above_threshold': is_cpu_usage_above_threshold
+    }
 
+def get_mem_health(config):
+    current_mem_stats = get_mem_info()
+    mem_load = current_mem_stats['virtual_memory_util_percent']
+    if mem_load > config['checks']['memory_usage_percent']:
+        is_mem_usage_above_threshold = True
+    else:
+        is_mem_usage_above_threshold = False
+    
+    return {
+        'mem_load': mem_load,
+        'above_threshold': is_mem_usage_above_threshold
+    }
 
+def get_disk_health(config):
+    current_disk_stats = get_disk_info()
+    disk_load = current_disk_stats['usage_percent']
+    if disk_load > config['checks']['disk_usage_percent']:
+        is_disk_usage_above_threshold = True
+    else:
+        is_disk_usage_above_threshold = False
+    
+    return {
+        'disk_load': disk_load,
+        'above_threshold': is_disk_usage_above_threshold
+    }
+
+def get_service_health(service):
+    try:
+        systemctl_result = subprocess.run(['systemctl', 'is-active', service], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        systemctl_status = systemctl_result.stdout.strip()
+        if systemctl_status == "active":
+            return {
+                'available': True
+            }
+        else:
+            return {
+                'available': False
+            }
+    except FileNotFoundError:
+        logger.error('ERROR: Failed to run systemctl on this system. Please make sure systemd is installed and working properly.')
+        sys.exit(1)
+
+def create_log_handlers(config, is_verbose_mode):
+    if is_verbose_mode == True:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(log_format)
+        logger.addHandler(console_handler)
+    generic_log_handler = logging.FileHandler(config['log_file'])
+    generic_log_handler.setLevel(logging.INFO)
+    generic_log_handler.setFormatter(log_format)
+    logger.addHandler(generic_log_handler)
+    alert_log_handler = logging.FileHandler(config['alert_log'])
+    alert_log_handler.setLevel(logging.WARNING)
+    alert_log_handler.setFormatter(log_format)
+    logger.addHandler(alert_log_handler)
+    syslog_handler = SysLogHandler(address='/dev/log')
+    syslog_handler.setLevel(logging.WARNING)
+    syslog_handler.setFormatter(log_format)
+    logger.addHandler(syslog_handler)
+
+def collect_all(config):
+    health_stats = {}
+    health_stats['cpu'] = get_cpu_health(config)
+    health_stats['mem'] = get_mem_health(config)
+    health_stats['disk'] = get_disk_health(config)
+    for service in config['checks']['services']:
+        health_stats['services'][service] = get_service_health(service)
+    
+    return health_stats
+
+def log_health_events(config, health_stats):
+    if health_stats['cpu']['above_threshold'] == True:
+        logger.warning(f'CPU 1 minute load average at {health_stats['cpu']['cpu_load']}, expected {config['checks']['cpu_load_1min']}')
+    if health_stats['mem']['above_threshold'] == True:
+        logger.warning(f'Memory usage at {health_stats['mem']['mem_load']}%, expected {config['checks']['memory_usage_percent']}%')
+    if health_stats['disk']['above_threshold'] == True:
+        logger.warning(f'Disk usage at {health_stats['disk']['disk_load']}%, expected {config['checks']['disk_usage_percent']}%')
+    for service in health_stats['services']:
+        if health_stats['services'][service]['available'] == False:
+            logger.error(f'System service {service} is not available!!!')
 
 
 if __name__ == '__main__':
